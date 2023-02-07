74c870e0ec480dcd191b7d1adf3e5ea330c066ef
Macko1994 <macko682@gmail.com>
2023-02-07 22:05:38 +0100

    invoice-generator - removed menu from invoice components and moved to app component

--- src/app/components/invoice-generator/invoice-generator.module.ts
+++ src/app/components/invoice-generator/invoice-generator.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {MenuModule} from "../menu/menu.module";
 
 import {NewInvoiceComponent} from "./components/new-invoice/new-invoice.component";
 import {InvoicePreviewComponent} from "./components/invoice-preview/invoice-preview.component";
@@ -13,7 +12,6 @@ import {InvoicePreviewComponent} from "./components/invoice-preview/invoice-prev
   ],
   imports: [
     CommonModule,
-    MenuModule
   ],
   exports: [
     NewInvoiceComponent,
