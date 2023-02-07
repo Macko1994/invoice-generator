c2e3cafd3cd1bf77ebc3d212f3e8808d2a087d6d
Macko1994 <macko682@gmail.com>
2023-02-07 21:23:29 +0100

    menu - created header nad menu, angular material was used

---
+++ src/app/components/menu/menu.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {SideMenuComponent} from "./components/side-menu/side-menu.component";
+import {MaterialModule} from "../../material/material.module";
+import {HeaderModule} from "../header/header.module";
+import {AppRoutingModule} from "../../app-routing.module";
+
+
+
+@NgModule({
+  declarations: [
+    SideMenuComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    HeaderModule,
+    AppRoutingModule
+  ],
+  exports: [
+    SideMenuComponent
+  ]
+})
+export class MenuModule { }
